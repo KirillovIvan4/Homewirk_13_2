04512dddf718d9cb9cd0bd732575c02ffe65515f
Kirillov_Ivan <kirillovio@mail.ru>
2024-03-19 05:58:37 +0300

    test

--- src/class_Category.py
+++ src/class_Category.py
@@ -10,41 +10,10 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.nummer_of_category += 1
-        Category.nummer_of_products += len(self.products)
-
-
-    def __repr__(self):
-        return f"{self.name} \n{self.products} \n"
-
-# class Product:
-#     name : str
-#     description : str
-#     price : float
-#     quantity : int
-#     def __init__(self,name ,description ,price ,quantity):
-#         self.name = name
-#         self.description = description
-#         self.price = price
-#         self.quantity =quantity
-#
-#     def __repr__(self):
-#         return self.name
-
-
-# data_category = utils.get_list()
-# list_category = []
-#
-# for i in range (len(data_category)):
-#     list_products = []
-#     for j in range (len(data_category[i]['products'])):
-#         #print(data_category[i]['products'][j])
-#         path_product = data_category[i]['products'][j]
-#         product = Product(path_product['name'], path_product['description'], path_product['price'], path_product['quantity'])
-#         list_products.append(product)
-#
-#     category = Category(data_category[i]['name'], data_category[i]['description'], data_category[i]['products'])
-#     list_category.append(category)
-# print(list_category)
-#
+        # Category.nummer_of_products += len(self.products)
+    #
+    #
+    # def __repr__(self):
+    #     return f"{self.name} \n{self.products[0]} \n"
--- src/main.py
+++ src/main.py
@@ -1,17 +1,25 @@
 from utils import utils
 import class_Category,class_Product
 
+# Сохраняю json файл в data_category
 data_category = utils.get_list()
+# Создаю пустой список list_category
 list_category = []
 
+# Запускаю цикл чтоба перебраль индексы категорий
 for i in range (len(data_category)):
+    # Создаю пустой список list_products в нутри цикла бля его очистки после итерации списка
     list_products = []
+    # Запускаю цикл чтоба перебраль индексы продуктов
     for j in range (len(data_category[i]['products'])):
-        #print(data_category[i]['products'][j])
         path_product = data_category[i]['products'][j]
+        # Добавляю в list_products объекты класса Product
         product = class_Product.Product(path_product['name'], path_product['description'], path_product['price'], path_product['quantity'])
         list_products.append(product)
-
-    category = class_Category.Category(data_category[i]['name'], data_category[i]['description'], data_category[i]['products'])
+    print(list_products)
+    products = list_products
+    #category = class_Category.Category(data_category[i]['name'], data_category[i]['description'], data_category[i]['products'])
+    category = class_Category.Category(data_category[i]['name'], data_category[i]['description'], products)
     list_category.append(category)
-print(list_category)
+print(list_category[0].products)
+
